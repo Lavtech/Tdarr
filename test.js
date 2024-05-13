66268870648bdc175a83f0c519591b61fc24135a
Lavtech <john@lavtechcomputers.com>
2024-05-13 14:52:08 -0700

    .

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const plugin = (file, librarySettings, inputs, otherArguments) => {
-  const lib = require('../methods/lib')();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
-  inputs = lib.loadDefaultValues(inputs, details);
-  const response = {
-    processFile: true,
-    preset: '',
-    handBrakeMode: false,
-    container: `.mkv`,
-    FFmpegMode: true,
-    reQueueAfter: true,
-    infoLog: '',
-  };
+// const plugin = (file, librarySettings, inputs, otherArguments) => {
+//   const lib = require('../methods/lib')();
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
+//   inputs = lib.loadDefaultValues(inputs, details);
+//   const response = {
+//     processFile: true,
+//     preset: '',
+//     handBrakeMode: false,
+//     container: `.mkv`,
+//     FFmpegMode: true,
+//     reQueueAfter: true,
+//     infoLog: '',
+//   };
 //original_container = `.${file.container}`,
 
 // Specific log to confirm the script is executing
 console.log("13421234243 This is the GitHubScript");
-response.infoLog += ' 13421234243 This is the GitHubScript. \n';
+//response.infoLog += ' 13421234243 This is the GitHubScript. \n';
 
 
   // Convert file if convert variable is set to true.
