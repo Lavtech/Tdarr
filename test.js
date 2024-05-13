aa45143ee3c5ab984c3849cd869cb29fc7f58306
Lavtech <john@lavtechcomputers.com>
2024-05-13 13:25:26 -0700

    .

--- test.js
+++ test.js
@@ -16,46 +16,13 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
 console.log("13421234243 This is the GitHubScript");
 response.infoLog += '☒ 13421234243 This is the GitHubScript. \n';
 
-  // Check if file is a video. If it isn't then exit plugin.
-  if (file.fileMedium !== 'video') {
-    response.processFile = false;
-    response.infoLog += '☒File is not a video. \n';
-    return response;
-  }
-
-  // Set up required variables.
-  let videoIdx = 0;
-  let extraArguments = '';
-  let convert = false;
-
-  // Go through each stream in the file.
-  for (let i = 0; i < file.ffProbeData.streams.length; i++) {
-    // Check if stream is video.
-    if (file.ffProbeData.streams[i].codec_type.toLowerCase() === 'video') {
-      // Check if stream codec is mjpeg, png or gif. Remove if so.
-      if (
-        file.ffProbeData.streams[i].codec_name === 'mjpeg'
-        || file.ffProbeData.streams[i].codec_name === 'png'
-        || file.ffProbeData.streams[i].codec_name === 'gif'
-      ) {
-        convert = true;
-        extraArguments += `-map -v:${videoIdx} `;
-      }
-      // Increment videoIdx.
-      videoIdx += 1;
-    }
-  }
 
   // Convert file if convert variable is set to true.
-  if (convert === true) {
+
     response.preset += `,-map 0 -c copy -max_muxing_queue_size 9999 ${extraArguments}`;
     response.infoLog += '☒File has image format stream, removing. \n';
     response.processFile = true;
-  } else {
-    response.processFile = false;
-    response.infoLog
-      += "☑File doesn't contain any unwanted image format streams.\n";
-  }
+
   return response;
 };
 
