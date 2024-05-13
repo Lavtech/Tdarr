91c2cea81a2c30d06993f45926a7cd5cf3f274ee
Lavtech <john@lavtechcomputers.com>
2024-05-13 14:53:02 -0700

    .

--- test.js
+++ test.js
@@ -16,7 +16,7 @@
 
 // Specific log to confirm the script is executing
 console.log("13421234243 This is the GitHubScript");
-//response.infoLog += ' 13421234243 This is the GitHubScript. \n';
+response.infoLog += ' 13421234243 This is the GitHubScript. \n';
 
 
   // Convert file if convert variable is set to true.
