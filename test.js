8065e9800d6bc9be460d729bed5565082bfa5cf7
Lavtech <john@lavtechcomputers.com>
2024-05-13 12:32:22 -0700

    .

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+const plugin = (file, librarySettings, inputs, otherArguments) => {
+    const importFresh = require('import-fresh');
+    const library = importFresh('../methods/library.js');
+    
+      //Must return this object at some point
+      const response = {
+         processFile : false,
+         preset : '',
+         container : '.mkv',
+         handbrakeMode : false,
+         ffmpegMode : true,
+         reQueueAfter : true,
+         infoLog : '',
+    
+      }
+
 response.infoLog += '☒ 13421234243 This is the GitHubScript. \n';
