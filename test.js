baa1d5479cf0c33e98ad7056bde4b9fa75437488
Lavtech <john@lavtechcomputers.com>
2024-05-13 12:48:45 -0700

    .

--- test.js
+++ test.js
@@ -1,24 +1,29 @@
-// eslint-disable-next-line no-unused-vars
+// Assume the presence of some essential Node.js modules if needed
+const importFresh = require('import-fresh');
+
+// Plugin function that matches the interface expected by the Tdarr server script
 const plugin = (file, librarySettings, inputs, otherArguments) => {
-    const importFresh = require('import-fresh');
+    // Assuming importFresh is used to ensure fresh module instances (if necessary)
+    // For demonstration, we assume the module path is correct or adjusted as needed
     const library = importFresh('../methods/library.js');
-    
-      //Must return this object at some point
-      const response = {
-         processFile : false,
-         preset : '',
-         container : '.mkv',
-         handbrakeMode : false,
-         ffmpegMode : true,
-         reQueueAfter : true,
-         infoLog : '',
-    
-      }
 
-
-
-
-response.infoLog += '☒ 13421234243 This is the GitHubScript. \n';
+    // Prepare the response structure as expected by Tdarr
+    const response = {
+        processFile: false,  // No processing needed, just a demo
+        preset: '',
+        container: '.mkv',
+        handbrakeMode: false,
+        ffmpegMode: true,
+        reQueueAfter: true,
+        infoLog: ''
     };
+
+    // Specific log to confirm the script is executing
+    console.log("13421234243 This is the GitHubScript");
+    response.infoLog += '☒ 13421234243 This is the GitHubScript. \n';
+
+    // Returning the response object which Tdarr expects
+    return response;
+};
 module.exports.details = details;
 module.exports.plugin = plugin;
