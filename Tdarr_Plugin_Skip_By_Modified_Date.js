fe29866e6447dc6fc173582afddb1bb7f4309b60
Lavtech <82127328+Lavtech@users.noreply.github.com>
2024-10-29 16:44:39 -0700

    Delete Tdarr_Plugin_Skip_By_Modified_Date.js

--- Tdarr_Plugin_Skip_By_Modified_Date.js
+++
@@ -1,9 +0,0 @@
-const details = () => ({
-    id: "Tdarr_Plugin_skip_By_Modified_Date",
-    Name: "Skip by modified date",
-    Type: "Video",
-    Operation: "Filter",
-    Description: "This will not do anything if the modified date is older than specified date",
-    Version: "",
-    Link: ""
-});
